e6608c8325ab01b16afb5407d373e4a05d12e10b
Shreyas <shreyas.anandgs@gmail.com>
2016-03-10 16:14:33 -0600

    Updating mongodb url to host on heroku

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@
 
 // expose the mongodb connection url to our app with module.exports
 module.exports = {
-    url : 'mongodb://localhost:27017/todo'
+    urlHeroku : 'mongodb://heroku_eaitododb:@eaimongodb1@ds011409.mlab.com:11409/heroku_vm5mmdxg',
+    urlLocal : 'mongodb://localhost:27017/todo'
 };
