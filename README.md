a0f5af9a7f52c0b435b5b6d03efb5cc5e6931382
Shreyas <shreyas.anandgs@gmail.com>
2016-03-10 02:12:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# A MEAN stack Todo app
+
+A simple todo app built with the MEAN (MongoDB, Express, Angular, Node) stack.
+
+    - Node provides the RESTful API.
+    - Angular provides the frontend and accesses the API. 
+    - MongoDB provides the datastore.
+    - Express helps in creating the API routes
+
+## Requirements
+
+- [Node and npm](http://nodejs.org)
+- [MongoDB](https://www.mongodb.org/)
+
+## Installation
+
+1. Clone the repository: `git clone git@github.com:scotch-io/node-todo`
+2. Install the application: `npm install`
+3. Start the server: `node server.js`
+4. View in browser at `http://localhost:3000`
+
+## App features
+
+1. Add a todo
+2. Mark a todo as completed/ Unmark a todo as still active (toggle state)
+3. Delete a todo
---
+++ server.js
@@ -0,0 +1,117 @@
+// server.js
+
+    // set up ========================
+    var express  = require('express');
+    var app      = express();                               // create our app w/ express
+    var mongoose = require('mongoose');                     // mongoose for mongodb
+    var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+    // configuration =================
+
+    mongoose.connect('mongodb://localhost:27017/todo');     // connect to mongoDB database
+
+    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
+    // define model =========
+    var Todo = mongoose.model('Todo', {
+        text : String,
+        active: Boolean
+    });
+
+    // application -------------------------------------------------------------
+    app.get('/api/', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+
+    // listen (start app with node server.js) ======================================
+    app.listen(3000);
+    console.log("App listening on port 3000");
+
+    // routes ================================
+    
+        // api -------------------------------
+        // get all todos
+        app.get('/api/todos', function(req, res) {
+
+            // use mongoose to get all todos in the database
+            Todo.find(function(err, todos) {
+
+                // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+                if (err)
+                    res.send(err)
+
+                res.json(todos); // return all todos in JSON format
+            });
+        });
+
+        // create todo and send back all todos after creation
+        app.post('/api/todos', function(req, res) {
+
+            // create a todo, information comes from AJAX request from Angular
+            Todo.create({
+                text : req.body.text,
+                active: true,
+                done : false
+            }, function(err, todo) {
+                if (err)
+                    res.send(err);
+
+                // get and return all the todos after you create another
+                Todo.find(function(err, todos) {
+                    if (err)
+                        res.send(err)
+                    res.json(todos);
+                });
+            });
+        });
+
+        // toggle todo active status
+        app.put('/api/todos/:todo_id', function(req, res) {
+            
+            // find the todo and update it
+            Todo.findById(req.params.todo_id, function(err, todo) {
+                
+                if(err)
+                    res.send(err);
+                
+                todo.active = !todo.active;
+                
+                // save the todo
+                todo.save(function(err) {
+                    if(err)
+                        res.send(err);
+                    
+                    // get and return all the todos after you remove
+                    Todo.find(function(err, todos) {
+                        if (err)
+                            res.send(err)
+                        res.json(todos);
+                    });
+                });
+            });
+        });
+
+
+        // delete a todo
+        app.delete('/api/todos/:todo_id', function(req, res) {
+            Todo.remove({
+                _id : req.params.todo_id
+            }, function(err, todo) {
+                if (err)
+                    res.send(err);
+
+                // get and return all the todos after you remove
+                Todo.find(function(err, todos) {
+                    if (err)
+                        res.send(err)
+                    res.json(todos);
+                });
+            });
+        });
